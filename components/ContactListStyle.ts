ed4b83740a8436008fa71db29018800a2bfc02bd
Ritik kumar verma <hritikverma2575@gmail.com>
2024-02-28 01:54:38 +0530

    looping array styles

--- components/ContactListStyle.ts
+++ components/ContactListStyle.ts
@@ -2,19 +2,40 @@ import { StyleSheet } from "react-native";
 
 const ContactListStyle=StyleSheet.create({
      headingText:{
-        fontSize:40,
+        fontSize:24,
+        fontWeight:'bold',
+        paddingHorizontal:8
      },
      container:{
+        paddingHorizontal:16,
+        marginBottom:4,
 
      },
-     userCard:{},
+     userCard:{
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        marginBottom:4,
+        backgroundColor:'#8D3DAF',
+        padding:8,
+        borderRadius:10
+
+     },
      userImage:{
         width:60,
         height:60,
-        borderRadius: 60/2
+        borderRadius: 60/2,
+        marginRight:14,
      },
-     userName:{},
-     userStatus:{},
+     userName:{
+        fontSize:16,
+        fontWeight:'600',
+        color:'#fff',
+
+     },
+     userStatus:{
+        fontSize:12,
+     },
 })
 
 export default ContactListStyle
